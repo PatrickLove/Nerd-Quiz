e96acddbedb3efb709aa7abbe10531751bc4a986
PatrickLove <patricklove@comcast.net>
2014-08-02 10:26:06 -0400

    Project Setup

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+    app = express(),
+    path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.listen(3000);
