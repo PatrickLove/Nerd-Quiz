bb16bdb7f0d8167a6c3b12d13805423853d333b6
PatrickLove <patricklove@comcast.net>
2014-08-02 11:04:27 -0400

    jQuery and unformatted index.html

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res){
+    res.redirect('/index.html');
+});
+
 app.listen(3000);
